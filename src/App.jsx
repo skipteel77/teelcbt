f06a329f790936430a25aa862015e02d0d17e83f
Skip Teel <skipteel@mac.lan>
2026-09-11 11:30:15 -0400

    added SEO optimizations

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
-import { Routes, Route, useLocation } from 'react-router-dom'
-import { useEffect } from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import About from './pages/About'
-import Services from './pages/Services'
-import Approach from './pages/Approach'
-import FAQ from './pages/FAQ'
-import Contact from './pages/Contact'
-import PrivacyPolicy from './pages/PrivacyPolicy'
-import NoticeOfPrivacyPractices from './pages/NoticeOfPrivacyPractices'
-import NotFound from './pages/NotFound'
+import { Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Approach from "./pages/Approach";
+import FAQ from "./pages/FAQ";
+import Contact from "./pages/Contact";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import NoticeOfPrivacyPractices from "./pages/NoticeOfPrivacyPractices";
+import NotFound from "./pages/NotFound";
+import SEO from "./components/SEO";
 
 function ScrollToTop() {
-  const { pathname } = useLocation()
+  const { pathname } = useLocation();
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [pathname])
-  return null
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
 }
 
 export default function App() {
   return (
     <>
       <ScrollToTop />
+      <SEO />
       <Header />
       <main>
         <Routes>
@@ -34,11 +36,14 @@ export default function App() {
           <Route path="/faq" element={<FAQ />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/notice-of-privacy-practices" element={<NoticeOfPrivacyPractices />} />
+          <Route
+            path="/notice-of-privacy-practices"
+            element={<NoticeOfPrivacyPractices />}
+          />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
       <Footer />
     </>
-  )
+  );
 }
